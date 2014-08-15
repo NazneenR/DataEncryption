8cdf472f7f07f5c3c8c8d7f6d1fcf3bfdf7499fa
NazneenRupawalla <nazneenr@thoughtworks.com>
2014-08-15 17:55:06 +0530

    Showing the values on UI.

--- src/cryptography/encryption/DataEncryptionActivity.java
+++ src/cryptography/encryption/DataEncryptionActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.String;
 
 public class DataEncryptionActivity extends Activity {
@@ -14,27 +16,39 @@ public class DataEncryptionActivity extends Activity {
 
 	private Button startProcessButton;
 	private DataEncryption encrypter;
+    private TextView textBox;
+    private TextView textBoxForDecryptedValue;
 
-	@Override
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.data_encryption_and_decryption);
 
 		Log.d(TAG, "onCreate called");
 		startProcessButton = (Button) findViewById(R.id.startButton);
+		textBox = (TextView) findViewById(R.id.editText4);
+		textBoxForDecryptedValue = (TextView) findViewById(R.id.editText6);
+
 		startProcessButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                encryptAndDecryptString();
+                try {
+                    encryptAndDecryptString(textBox,textBoxForDecryptedValue);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-	private void encryptAndDecryptString() {
+	private void encryptAndDecryptString(TextView textBox, TextView textBoxForDecryptedValue) throws UnsupportedEncodingException {
 		
 	    encrypter = new EncryptionUsingJavaCrypto();
 
 		byte[] encryptedString = encrypter.encryptString("Test String");
-		encrypter.decryptString(encryptedString);
+        textBox.setText(new String(encryptedString, "UTF-8"));
+
+        byte[] decrypted_bytes = encrypter.decryptString(encryptedString);
+        textBoxForDecryptedValue.setText(new String(decrypted_bytes, "UTF-8"));
 	}
 }
 
